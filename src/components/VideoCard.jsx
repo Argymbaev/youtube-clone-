d00108e948c043e8587ead226fb7a11547a6ed31
Nazar <121248117+Argymbaev@users.noreply.github.com>
2023-07-17 16:31:00 +0600

    first commit

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,37 +1,56 @@
 import React from "react";
-import { Stack } from "@mui/material";
-import ReactPlayer from 'react-player';
+import { Link } from "react-router-dom";
+import { Typography, Card, CardContent, CardMedia } from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
-import { categories } from "../utils/constants";
+import {
+  demoThumbnailUrl,
+  demoVideoUrl,
+  demoVideoTitle,
+  demoChannelUrl,
+  demoChannelTitle,
+} from "../utils/constants";
 
-const Categories = ({ selectedCategory, setSelectedCategory }) => (
-  <Stack
-    direction="row"
+const VideoCard = ({
+  video: {
+    id: { videoId },
+    snippet,
+  },
+}) => (
+  <Card
     sx={{
-      overflowY: "auto",
-      height: { sx: "auto", md: "95%" },
-      flexDirection: { md: "column" },
+      width: { xs: "100%", sm: "358px", md: "320px" },
+      boxShadow: "none",
+      borderRadius: 0,
     }}
   >
-    {categories.map((category) => (
-      <button
-        className="category-btn"
-        onClick={() => setSelectedCategory(category.name)}
-        style={{
-          background: category.name === selectedCategory && "#FC1503",
-          color: "white",
-        }}
-        key={category.name}
+    <Link to={videoId ? `/video/${videoId}` : `/video/cV2gBU6hKfY`}>
+      <CardMedia
+        image={snippet?.thumbnails?.high?.url || demoThumbnailUrl}
+        alt={snippet?.title}
+        sx={{ width: { xs: "100%", sm: "358px" }, height: 180 }}
+      />
+    </Link>
+    <CardContent sx={{ backgroundColor: "#1E1E1E", height: "106px" }}>
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+          {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+        </Typography>
+      </Link>
+      <Link
+        to={
+          snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl
+        }
       >
-        <span style={{ color: category.name === selectedCategory ? "white" : "red", marginRight: "15px" }}>
-          {category.icon}
-        </span>
-        <span style={{ opacity: category.name === selectedCategory ? "1" : "0.8" }}>
-          {category.name}
-        </span>
-      </button>
-    ))}
-  </Stack>
+        <Typography variant="subtitle2" color="gray">
+          {snippet?.channelTitle || demoChannelTitle}
+          <CheckCircleIcon
+            sx={{ fontSize: "12px", color: "gray", ml: "5px" }}
+          />
+        </Typography>
+      </Link>
+    </CardContent>
+  </Card>
 );
 
-export default Categories;
+export default VideoCard;
